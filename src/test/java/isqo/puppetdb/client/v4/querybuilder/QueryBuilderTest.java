fbebc1aed48b952d2943edc6573839fd1bd574f4
isqo <qouiqaismail@gmail.com>
2018-06-03 23:27:42 +0200

    Initialize AST querybuilder module

---
+++ src/test/java/isqo/puppetdb/client/v4/querybuilder/QueryBuilderTest.java
@@ -0,0 +1,12 @@
+package isqo.puppetdb.client.v4.querybuilder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class QueryBuilderTest {
+    @Test
+    public void selectAll(){
+        fail("first tdd unit test");
+    }
+}
