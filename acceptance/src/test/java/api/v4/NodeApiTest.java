2cb2ee991641395d35b2251c388f1251318fa1a1
isqo <qouiqaismail@gmail.com>
2020-02-17 12:58:57 +0100

    create a separate acceptance tests module

---
+++ acceptance/src/test/java/api/v4/NodeApiTest.java
@@ -0,0 +1,10 @@
+package api.v4;
+
+import org.junit.jupiter.api.Test;
+
+public class NodeApiTest {
+    @Test
+    public void dummyTest() {
+        org.junit.jupiter.api.Assertions.assertTrue(true);
+    }
+}
