830b7d9c13d17017ddcfb0ab0bf2b467a1984748
isqo <qouiqaismail@gmail.com>
2020-02-20 00:32:25 +0100

    support classic binary operators in AstQueryBuilder

--- src/main/java/isqo/puppetdb/client/v4/querybuilder/AstQueryBuilder.java
+++ src/main/java/isqo/puppetdb/client/v4/querybuilder/AstQueryBuilder.java
@@ -2,19 +2,44 @@ package isqo.puppetdb.client.v4.querybuilder;
 
 public class AstQueryBuilder {
     private static final String EQUAL_OP = "=";
+    public static final String GREATERTHAN_OP = ">";
+    public static final String LESS_THAN_OP = "<";
     private String field;
     private String value;
     private String operator;
     private String queryStringFormat = "[\"%s\",\"%s\",\"%s\"]";
 
-    public AstQueryBuilder equal(String field, String value) {
+    public AstQueryBuilder(String field, String value) {
         this.field = field;
         this.value = value;
+    }
+
+    public AstQueryBuilder equal() {
         this.operator = EQUAL_OP;
+        return this;
+    }
+
+    public AstQueryBuilder greaterThan() {
+        this.operator = GREATERTHAN_OP;
         return this;
     }
 
     public String build() {
         return String.format(queryStringFormat, operator, field, value);
+    }
+
+    public AstQueryBuilder lessThan() {
+        this.operator = LESS_THAN_OP;
+        return this;
+    }
+
+    public AstQueryBuilder greaterThanOrEq() {
+        this.operator = GREATERTHAN_OP + EQUAL_OP;
+        return this;
+    }
+
+    public AstQueryBuilder lessThanOrEq() {
+        this.operator = LESS_THAN_OP + EQUAL_OP;
+        return this;
     }
 }
--- src/test/java/isqo/puppetdb/client/v4/querybuilder/QueryBuilderTest.java
+++ src/test/java/isqo/puppetdb/client/v4/querybuilder/QueryBuilderTest.java
@@ -10,8 +10,44 @@ public class QueryBuilderTest {
     @DisplayName("ASTQueryBuilder should return a valid 3 elements query string for equal binary operator")
     public void equalOperator() {
         assertEquals("[\"=\",\"certname\",\"c9d2ddc6b309.us-east-2.compute.internal\"]",
-                new AstQueryBuilder()
-                        .equal("certname", "c9d2ddc6b309.us-east-2.compute.internal")
+                new AstQueryBuilder("certname", "c9d2ddc6b309.us-east-2.compute.internal")
+                        .equal()
+                        .build());
+    }
+
+    @Test
+    @DisplayName("ASTQueryBuilder should return a valid 3 elements query string for 'greater than' operator")
+    public void greaterThanOperator() {
+        assertEquals("[\">\",\"rubyversion\",\"2.4.3\"]",
+                new AstQueryBuilder("rubyversion", "2.4.3")
+                        .greaterThan()
+                        .build());
+    }
+
+    @Test
+    @DisplayName("ASTQueryBuilder should return a valid 3 elements query string for 'less than' operator")
+    public void lessThanOperator() {
+        assertEquals("[\"<\",\"rubyversion\",\"2.4.3\"]",
+                new AstQueryBuilder("rubyversion", "2.4.3")
+                        .lessThan()
+                        .build());
+    }
+
+    @Test
+    @DisplayName("ASTQueryBuilder should return a valid 3 elements query string for 'greater than or equal to' operator")
+    public void greaterThanOrEqualOperator() {
+        assertEquals("[\">=\",\"rubyversion\",\"2.4.3\"]",
+                new AstQueryBuilder("rubyversion", "2.4.3")
+                        .greaterThanOrEq()
+                        .build());
+    }
+
+    @Test
+    @DisplayName("ASTQueryBuilder should return a valid 3 elements query string for 'less than or equal to' operator")
+    public void lessThanOrEqualOperator() {
+        assertEquals("[\"<=\",\"rubyversion\",\"2.4.3\"]",
+                new AstQueryBuilder("rubyversion", "2.4.3")
+                        .lessThanOrEq()
                         .build());
     }
 }
