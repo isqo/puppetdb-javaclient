684706f6f77ed2195b444456d9083d04eaae41b9
isqo <qouiqaismail@gmail.com>
2020-03-19 20:39:54 +0100

    add first acceptance test for normal case

--- acceptance/src/test/java/api/v4/NodeApiTest.java
+++ acceptance/src/test/java/api/v4/NodeApiTest.java
@@ -1,10 +1,25 @@
 package api.v4;
 
+import isqo.puppetdb.client.v4.api.NodeData;
+import isqo.puppetdb.client.v4.api.NodeEndPoint;
+import isqo.puppetdb.client.v4.http.PdbHttpConnection;
+import isqo.puppetdb.client.v4.querybuilder.AstQueryBuilder;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NodeApiTest {
     @Test
-    public void dummyTest() {
-        org.junit.jupiter.api.Assertions.assertTrue(true);
+    @DisplayName("puppetdb should responds correctly for c826a077907a.us-east-2.compute.internal node")
+    public void normalCase1() {
+      NodeEndPoint endPoint = new NodeEndPoint(new PdbHttpConnection().setHost("puppetdb").setPort(8080));
+      NodeData node = endPoint.getData(new AstQueryBuilder("certname").equals("c826a077907a.us-east-2.compute.internal").build());
+      assertEquals("production", node.getFactsEnvironment());
+      assertEquals("production", node.getCatalogEnvironment());
+      assertEquals("2020-02-15T22:25:53.219Z", node.getFactsTimestamp());
+      assertEquals("c826a077907a.us-east-2.compute.internal", node.getCertname());
+      assertEquals("2020-02-15T22:25:53.744Z", node.getCatalogTimestamp());
     }
 }
